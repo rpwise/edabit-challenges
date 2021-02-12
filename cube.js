cd610a8ab84ca271d701bfc950c175aff2f206ce
Rick Wise <rpwise@gmail.com>
2021-02-12 11:36:06 -0500

    added cube function and unit tests

---
+++ cube.js
@@ -0,0 +1,4 @@
+function cube(x){
+    return x * x * x;
+}
+module.exports = cube;
---
+++ cube.test.js
@@ -0,0 +1,13 @@
+const cube = require('./cube.js');
+
+test('cube of 1 to equal 1', () => {
+    expect(cube(1)).toBe(1);
+  });
+  
+  test('cube of 3 to equal 9', () => {
+    expect(cube(3)).toBe(27);
+  });
+
+  test('cube of 5 to equal 125', () => {
+    expect(cube(5)).toBe(125);
+  });
