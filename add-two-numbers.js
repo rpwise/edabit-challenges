1d4fa0d306d5f1ba6eae476cf4fccfb6eefe7a7d
Rick Wise <rpwise@gmail.com>
2021-02-12 12:33:43 -0500

    added null handling for add two numbers

--- add-two-numbers.js
+++ add-two-numbers.js
@@ -1,4 +1,7 @@
 function sum(a, b) {
+  if (a == null || b == null){
+    throw "Parameter is null";
+  }
   return a + b;
 }
 module.exports = sum;
--- add-two-numbers.test.js
+++ add-two-numbers.test.js
@@ -7,3 +7,21 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds 0 + -1 to equal -1', () => {
   expect(sum(0, -1)).toBe(-1);
 });
+
+test('add null', () => {
+  expect(() => { 
+      sum(1, null);
+  }).toThrow("Parameter is null");
+});
+
+test('add null', () => {
+  expect(() => { 
+      sum(null, null);
+  }).toThrow("Parameter is null");
+});
+
+test('add null', () => {
+  expect(() => { 
+      sum(null, 1);
+  }).toThrow("Parameter is null");
+});
